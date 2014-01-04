669a69fa720681b044d2f1671cd67300ce534aa5
Zhang, Xiaolong <xiaolozhang@paypal.com>
2014-01-04 14:31:58 -0800

    Rebuilding from Scratch
    creating-the-project-and-listactivity
    handling-a-lack-of-data
    adding-data-for-the-list
    all-about-string-resources

--- src/com/xiaolong/blogreader/MainListActivity.java
+++ src/com/xiaolong/blogreader/MainListActivity.java
@@ -1,15 +1,24 @@
 package com.xiaolong.blogreader;
 
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
-public class MainListActivity extends Activity {
+public class MainListActivity extends ListActivity {
+
+	/*protected String[] mAndroidNames = { "Froyo", "Gingerbread", "Honeycomb",
+			"Ice Cream Sandwich", "Jelly Bean", "KitKat" };*/
+	protected String[] mAndroidNames;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_list);
+		mAndroidNames = getResources().getStringArray(R.array.android_names);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mAndroidNames);
+		setListAdapter(adapter);
+//		Toast.makeText(this, getString(R.string.no_items), Toast.LENGTH_LONG).show();
 	}
 
 	@Override
