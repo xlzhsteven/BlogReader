507cec49ea7c847895035848c0207c320a2cfc7a
Zhang, Xiaolong <xiaolozhang@paypal.com>
2014-01-05 12:00:27 -0800

    Request URL response
    URL response and extends AsyncTask as subclass
    Change Internet Permission in the AndroidManifest file

--- src/com/xiaolong/blogreader/MainListActivity.java
+++ src/com/xiaolong/blogreader/MainListActivity.java
@@ -1,24 +1,38 @@
 package com.xiaolong.blogreader;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.ListActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
-import android.widget.ArrayAdapter;
 
 public class MainListActivity extends ListActivity {
 
-	/*protected String[] mAndroidNames = { "Froyo", "Gingerbread", "Honeycomb",
-			"Ice Cream Sandwich", "Jelly Bean", "KitKat" };*/
-	protected String[] mAndroidNames;
+	/*
+	 * protected String[] mAndroidNames = { "Froyo", "Gingerbread", "Honeycomb",
+	 * "Ice Cream Sandwich", "Jelly Bean", "KitKat" };
+	 */
+	protected String[] mBlogPostTitles;
+
+	// Create variable to use to get info from the website
+	public static final int NUMBER_OF_POSTS = 20;
+	public static final String TAG = MainListActivity.class.getSimpleName();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_list);
-		mAndroidNames = getResources().getStringArray(R.array.android_names);
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mAndroidNames);
-		setListAdapter(adapter);
-//		Toast.makeText(this, getString(R.string.no_items), Toast.LENGTH_LONG).show();
+
+		GetBlogPostTask getBlogPostTask = new GetBlogPostTask();
+		getBlogPostTask.execute();
+
+		// Toast.makeText(this, getString(R.string.no_items),
+		// Toast.LENGTH_LONG).show();
 	}
 
 	@Override
@@ -26,6 +40,36 @@ public class MainListActivity extends ListActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main_list, menu);
 		return true;
+	}
+
+	private class GetBlogPostTask extends AsyncTask<Object, Void, String> {
+
+		@Override
+		protected String doInBackground(Object... params) {
+			int responseCode = -1;
+			// TODO Auto-generated method stub
+			try {
+				URL blogFeedURL = new URL(
+						"http://blog.teamtreehouse.com/api/get_recent_summary/?count="
+								+ NUMBER_OF_POSTS);
+				HttpURLConnection connection = (HttpURLConnection) blogFeedURL
+						.openConnection();
+				connection.connect();
+				responseCode = connection.getResponseCode();
+				Log.i(TAG, "Code" + responseCode);
+			} catch (MalformedURLException e) {
+				// TODO: handle exception
+				Log.e(TAG, "Exception Caught: ", e);
+			} catch (IOException e) {
+				// TODO: handle exception
+				Log.e(TAG, "Exception Caught", e);
+			} catch (Exception e) {
+				// TODO: handle exception
+				Log.e(TAG, "Exception Caught", e);
+			}
+			return "Code" + responseCode;
+		}
+
 	}
 
 }
