ad56d8ca27f6ada3c13ce8f7560dc70274e5baa2
Zhang, Xiaolong <xiaolozhang@paypal.com>
2014-01-07 23:21:54 -0800

    Handle different versions of android

--- gen/com/xiaolong/blogreader/R.java
+++ gen/com/xiaolong/blogreader/R.java
@@ -25,10 +25,10 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_action_share=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_action_share_dark=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
         public static final int action_share=0x7f090002;
         public static final int progressBar1=0x7f090001;
         public static final int webView1=0x7f090000;
@@ -39,7 +39,6 @@ public final class R {
     }
     public static final class menu {
         public static final int blog_web_view=0x7f080000;
-        public static final int main_list=0x7f080001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
